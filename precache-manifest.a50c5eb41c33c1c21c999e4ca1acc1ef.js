e59604d33b5a40fda0198e52d909c17493cef651
Vadim Alkhimenok <vad.alkhimenok@gmail.com>
2020-09-13 16:29:31 +0300

    Updates

---
+++ precache-manifest.a50c5eb41c33c1c21c999e4ca1acc1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902fc4585a9b7741634eaae7fbea5228",
+    "url": "/gh-pages-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "1c95ee92e55502271ab9",
+    "url": "/gh-pages-tic-tac-toe/static/css/main.79f9f2cc.chunk.css"
+  },
+  {
+    "revision": "d3611154c896a7afa609",
+    "url": "/gh-pages-tic-tac-toe/static/js/2.c6535913.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-tic-tac-toe/static/js/2.c6535913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c95ee92e55502271ab9",
+    "url": "/gh-pages-tic-tac-toe/static/js/main.f07e3b86.chunk.js"
+  },
+  {
+    "revision": "81309cb8454c0472e2f4",
+    "url": "/gh-pages-tic-tac-toe/static/js/runtime-main.2bf67e97.js"
+  }
+]);
